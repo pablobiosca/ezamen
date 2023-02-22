05be5798d57d0a70083efa0995493cb3a38a5193
chabaking <zaslake@gmail.com>
2023-02-22 15:35:16 +0100

    preparado para deployear

--- database.js
+++ database.js
@@ -1,19 +1,36 @@
 const mysql = require("mysql2/promise")
 
-const pool = mysql.createPool({
-    host:"bzcvslh9cpp6tdzsqeif-mysql.services.clever-cloud.com",
-    password:"v2L0hwnf0QvwJj2RgDVC",
-    user:"u4ffzd7fekuncb52",
-    database:"bzcvslh9cpp6tdzsqeif"
-})
+//volcamos al process env
+require("dotenv").config()
 
-// let datos = {url:"xx",titulo:"locoo222",likes:10,dislikes:9}
+let pool
 
-// let con = async () =>{
-//     console.log(await pool.query("insert into fotos set ?",[datos]))
-// } 
+if (process.env.NODE_ENV=="develop"){
+    pool = mysql.createPool({
+        host:"localhost",
+        password:"1234",
+        user:"root",
+        database:"fotos",
+        port:9900
+    })
+    
+}else{
+    pool = mysql.createPool({
+        host:process.env.MYSQL_HOST,
+        password:process.env.MYSQL_PASSWORD,
+        user:process.env.MYSQL_USER,
+        database:process.env.MYSQL_DATABASE
+    })
+}
 
-// con()
+
+let datos = {url:"xx",titulo:"locoo222",likes:10,dislikes:9}
+
+let con = async () =>{
+    console.log(await pool.query("insert into fotos set ?",[datos]))
+} 
+
+con()
 
 
 
